fca1d3c8c15aefc819e8f13195323327fd0f4895
foollovingu <85272374+foollovingu@users.noreply.github.com>
2021-07-09 20:01:22 +0700

    Create config.js

--- config.js
+++ config.js
@@ -1,19 +1,19 @@
 const CONFIG = {
-    introTitle: 'Bé Na à!',
+    introTitle: 'Bé Dủm à!',
     introDesc: `Trái đất vốn lạ thường
-    Mà sao Na cứ đi nhầm đường
+    Mà sao Dủm cứ đi nhầm đường
     Lạc vào tim anh lẻ loi
     Đằng sau chữ yêu đây là thương`,
     btnIntro: 'hihi',
-    title: 'Phải chăng Na đã yêu ngay từ cái nhìn đầu tiên 😙',
-    desc: 'Phải chăng Na đã say ngay từ lúc thấy nụ cười ấy ',
+    title: 'Phải chăng Dủm đã yêu ngay từ cái nhìn đầu tiên 😙',
+    desc: 'Phải chăng Dủm đã say ngay từ lúc thấy nụ cười ấy ',
     btnYes: 'Vẫn cứ là thích anh <33',
-    btnNo: 'Không, Anh trai à :3',
-    question:'Trên thế giới hơn 7 tỉ người mà sao Na lại yêu anh <3',
+    btnNo: 'Không, Anh yêu à :3',
+    question:'Trên thế giới hơn 7 tỉ người mà sao Dủm lại yêu anh <3',
     btnReply: 'Gửi cho anh <3',
-    reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
-    mess: 'Anh biết mà 🥰. Yêu Na nhiều nhiều 😘😘',
-    messDesc: 'Tối nay đi uống trà sữa gần nhà hông a rước hihihi.',
+    reply: 'Dủm yêu anh nhiêu lắm hihi <33333333',
+    mess: 'Anh biết mà 🥰. Yêu Dủm nhiều nhiều 😘😘',
+    messDesc: 'Hết dịch đi uống trà sữa nha anh rước hihihi.',
     btnAccept: 'Okiiiii lun <3',
-    messLink: 'https://m.me/Ngothientin' //https://m.me/Ngothientin
+    messLink: 'https://m.me/truchaynoilinhtinh' //https://m.me/Ngothientin
 }

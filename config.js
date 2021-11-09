2a0e234814ebebc37a5c39e249d500ac6f4b5022
foollovingu <85272374+foollovingu@users.noreply.github.com>
2021-11-09 14:59:00 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,18 +1,18 @@
 const CONFIG = {
-    introTitle: 'Bé Dủm à!',
+    introTitle: 'Bé Mec à!',
     introDesc: `Trái đất vốn lạ thường
-    Mà sao Dủm cứ đi nhầm đường
+    Mà sao Mec cứ đi nhầm đường
     Lạc vào tim anh lẻ loi
     Đằng sau chữ yêu đây là thương`,
     btnIntro: 'hihi',
-    title: 'Phải chăng Dủm đã yêu ngay từ cái nhìn đầu tiên 😙',
-    desc: 'Phải chăng Dủm đã say ngay từ lúc thấy nụ cười ấy ',
+    title: 'Phải chăng Mec đã yêu ngay từ cái nhìn đầu tiên 😙',
+    desc: 'Phải chăng Mec đã say ngay từ lúc thấy nụ cười ấy ',
     btnYes: 'Vẫn cứ là thích anh <33',
     btnNo: 'Không, Anh yêu à :3',
-    question:'Trên thế giới hơn 7 tỉ người mà sao Dủm lại yêu anh <3',
+    question:'Trên thế giới hơn 7 tỉ người mà sao Mec lại yêu anh <3',
     btnReply: 'Gửi cho anh <3',
-    reply: 'Dủm yêu anh nhiêu lắm hihi <33333333',
-    mess: 'Anh biết mà 🥰. Yêu Dủm nhiều nhiều 😘😘',
+    reply: 'Mec yêu anh nhiêu lắm hihi <33333333',
+    mess: 'Anh biết mà 🥰. Yêuec nhiều nhiều 😘😘',
     messDesc: 'Hết dịch đi uống trà sữa nha anh rước hihihi.',
     btnAccept: 'Okiiiii lun <3',
     messLink: 'https://m.me/truchaynoilinhtinh' //https://m.me/Ngothientin
